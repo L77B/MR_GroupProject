4e6eeda826cb34698f75e05ba6404410c50d92f4
Bubblegumking10 <krishparikh64@gmail.com>
2026-05-10 14:26:09 +0200

    ZXing testing

--- Assets/Scripts/ZXingTest.cs
+++ Assets/Scripts/ZXingTest.cs
@@ -1,11 +1,27 @@
 using UnityEngine;
 using ZXing;
 
-public class ZXingTest : MonoBehaviour
+public class ZXingVersionTest : MonoBehaviour
 {
     void Start()
     {
+        // Test basic reader creation
         BarcodeReaderGeneric reader = new BarcodeReaderGeneric();
-        Debug.Log("ZXing loaded successfully!");
+        Debug.Log("BarcodeReaderGeneric created successfully");
+
+        // Log available methods
+        var methods = typeof(BarcodeReaderGeneric).GetMethods();
+        foreach (var method in methods)
+        {
+            if (method.Name == "Decode")
+            {
+                string parameters = "";
+                foreach (var param in method.GetParameters())
+                {
+                    parameters += $"{param.ParameterType.Name} {param.Name}, ";
+                }
+                Debug.Log($"Decode method found: Decode({parameters})");
+            }
+        }
     }
 }
