3e089f88c0d8d7bf43ed6e8b14f65a2c3cc66447
Bubblegumking10 <krishparikh64@gmail.com>
2026-05-21 16:57:04 +0200

    meshbreakable addon

--- Assets/Scripts/MeshExploderBreakable.cs
+++ Assets/Scripts/MeshExploderBreakable.cs
@@ -1,16 +1,82 @@
 using UnityEngine;
+using Fusion;
+using SBS.ME;
+using System;
 
-public class MeshExploderObject : MonoBehaviour
+public class MeshExploderBreakable : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Networked] private NetworkBool IsBroken
+        { get; set; }
+
+    private MeshExploder _exploder;
+    private Collider      _collider;
+
+    // Event for BreakableSpawner to subscribe to
+    public event Action<GameObject> OnBroken;
+
+    public override void Spawned()
     {
-        
+        _exploder = GetComponent<MeshExploder>();
+        _collider = GetComponent<Collider>();
+
+        if (_exploder == null)
+            Debug.LogError(
+                "MeshExploder not found!");
+
+        // Wire explosion finished event
+        if (_exploder != null)
+            _exploder.onExplosionFinished
+                .AddListener(OnExplosionFinished);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Called when bat hits this object
+    public void TakeHit()
     {
-        
+        if (IsBroken) return;
+
+        // Tell all clients to explode
+        RPC_Explode();
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    void RPC_Explode()
+    {
+        if (IsBroken) return;
+        IsBroken = true;
+
+        Debug.Log($"{gameObject.name} exploding!");
+
+        // Disable collider so no more hits
+        if (_collider != null)
+            _collider.enabled = false;
+
+        // Trigger mesh explosion
+        if (_exploder != null)
+            _exploder.EXPLODE();
+    }
+
+    void OnExplosionFinished()
+    {
+        // Notify BreakableSpawner
+        OnBroken?.Invoke(gameObject);
+
+        // Despawn from network
+        if (Object.HasStateAuthority)
+            StartCoroutine(DespawnAfterDelay());
+    }
+
+    System.Collections.IEnumerator DespawnAfterDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (Object != null && Object.IsValid)
+            Runner.Despawn(Object);
+    }
+
+    public override void Despawned(
+        NetworkRunner runner, bool hasState)
+    {
+        if (_exploder != null)
+            _exploder.onExplosionFinished
+                .RemoveListener(OnExplosionFinished);
     }
 }
